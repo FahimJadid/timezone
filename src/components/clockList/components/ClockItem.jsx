de445451392cdd54588cac3d35f0988cd24a573e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-09-10 02:38:34 +0600

    Update ClockItem to Display Dynamic Time

--- src/components/clockList/components/ClockItem.jsx
+++ src/components/clockList/components/ClockItem.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+import { formatInTimeZone } from "date-fns-tz";
+
 const ClockItem = ({ clock, setClocks }) => {
+  // console.log("time:", clock.time)
+  const [currentTime, setCurrentTime] = useState(clock.time);
+
+  // Update the clock's time every second to keep it dynamic
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formattedTime = formatInTimeZone(now, clock.timezone, "HH:mm:ss");
+
+      setCurrentTime(formattedTime);
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  }, [clock.timezone]);
+
   return (
     <div className="clock-item">
       <h3>
         {clock.title} <span>({clock.timezone})</span>
       </h3>
 
-      <p>Current Time: {clock.time}</p>
+      <p>Current Time: {currentTime}</p>
     </div>
   );
 };
