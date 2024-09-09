5ab27ea60f926e6819b0e5e8a384191d5d837f2c
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-09-10 05:11:31 +0600

    Add Event Validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,9 +4,19 @@ export const isDuplicateTitle = (title, clocks) => {
    return clocks.some((clock) => clock.title.toLowerCase() === title.toLowerCase())
 }
 
+// 
 
-// Basic validation for timezone 
+export const validateEvent = (events, newEvent) => {
+    if(!newEvent.title || !newEvent.time){
+        return {isValid: false, message: 'Event Title and time are required.'}
+    }
 
-export const isValidTimezone = (timezone) => {  
-    return timezone && timezone.trim().length > 0;
+    const eventExists = events.some((event) => event.title === newEvent.title)
+    if(eventExists){
+        return {isValid: false, message: 'An event with this title already exists.' };
+    }
+
+    return {isValid: true};
 }
+
+
